6895327e80b1c29e0af494391863f3c6644030ed
wisley18 <wisley@outlook.com>
2018-03-26 19:02:21 -0300

    Projeto Alura

---
+++ TestDrive/TestDrive/TestDrive/Views/DetalheView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestDrive.Views
+{
+	public partial class DetalheView : ContentPage
+	{
+        private const int FREIO_ABS = 800;
+        private const int AR_CONDICIONADO = 1000;
+        private const int MP3_PLAYER = 500;
+
+        public Veiculo Veiculo { get; set; }
+
+        public string TextoFreioABS
+        {
+            get
+            {
+                return string.Format("Freio ABS - R$ {0}", FREIO_ABS);
+            }
+        }
+
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return string.Format("Ar Condicionado - R$ {0}", AR_CONDICIONADO);
+            }
+        }
+
+        public string TextoMP3Player
+        {
+            get
+            {
+                return string.Format("MP3 Player - R$ {0}", MP3_PLAYER);
+            }
+        }
+
+        public DetalheView (Veiculo veiculo)
+		{
+			InitializeComponent ();
+            this.Veiculo = veiculo;
+
+            BindingContext = this;
+		}
+
+        private void ButtonProximo_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AgendamentoView(this.Veiculo));
+        }
+    }
+}
